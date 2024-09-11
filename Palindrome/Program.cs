ba1f73ae7860a609f8a0de5473354cb563205dd5
Lucas <115151663+Oceiroz@users.noreply.github.com>
2024-09-11 12:37:16 +0100

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInput("Write sumn to see if its a palindrome");
+        }
+        static void GetInput(string inputMessage)
+        {
+            Console.WriteLine($"{inputMessage}");
+            var rawInput = Console.ReadLine();
+            char[] inputList = rawInput.ToCharArray();
+            Array.Reverse(inputList);
+            string inputReverse = new string(inputList);
+            if (inputReverse == rawInput)
+            {
+                Console.WriteLine("This is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("This is not a palindrome");
+            }
+        }
+    }
+}
